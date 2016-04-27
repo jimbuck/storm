d6d097acf891750a56e5b19cffb6f4aeac730587
Jim Buck <jim@jimmyboh.com>
2016-04-27 17:07:35 -0400

    Fixed build, I think....

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('clean', function () {
 });
 
 function compileTypescript() {
-  return shell.task('node_modules\\.bin\\tsc.cmd');
+  return shell.task('node_modules\\.bin\\tsc');
 }
 
 gulp.task('compile', compileTypescript());
